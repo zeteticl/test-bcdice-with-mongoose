f7628953a5cc2e693e550863ce87f3c1c580c822
Zeteticl <zetet@dasd.com>
2020-09-25 20:34:02 +0800

    update

--- index.js
+++ index.js
@@ -3,15 +3,22 @@
 const BCDice = require('bcdice').default; 
 
 
-//npm install mongoose --save
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://testtest:testtest@zetetic.lxckb.mongodb.net/test?retryWrites=true&w=majority', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('we are connected!')
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+ 
+// Connection URL
+const url = 'mongodb+srv://testtest:testtest@zetetic.lxckb.mongodb.net/test?retryWrites=true&w=majority';
+ 
+// Database Name
+const dbName = 'test';
+ 
+// Use connect method to connect to the server
+MongoClient.connect(url, function(err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+ 
+  const db = client.db(dbName);
+ 
+  client.close();
 });
