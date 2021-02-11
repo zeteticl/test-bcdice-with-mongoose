12f01822993d1f48dd03806c70ef8d261ba367bf
Zeteticl <zetet@dasd.com>
2021-02-12 00:06:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-//npm install --save bcdice
-const BCDice = require('bcdice').default;
+const {
+  DynamicLoader
+} = require('bcdice');
 
-function getInfo(gameType) {
-  return BCDice.infoList.find(info => info.gameType === gameType);
+async function main() {
+  const loader = new DynamicLoader();
+
+  console.log(loader.listAvailableGameSystems().map(info => info.id));
+
+  const GameSystem = await loader.dynamicLoad('Cthulhu7th');
+  const result = GameSystem.eval('CC<=54');
+
+  console.log(result && result.text);
 }
 
-
-console.log(getInfo('Cthulhu'))
 
 
 
@@ -14,20 +20,19 @@ console.log(getInfo('Cthulhu'))
 //npm install mongodb --save
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
- 
+
 // Connection URL
 const url = 'mongodb+srv://testtest:testtest@zetetic.lxckb.mongodb.net/test?retryWrites=true&w=majority';
- 
+
 // Database Name
 const dbName = 'test';
- 
+
 // Use connect method to connect to the server
-MongoClient.connect(url, function(err, client) {
+MongoClient.connect(url, function (err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  client.close();
-});
 
+  const db = client.db(dbName);
+
+  main();
+});
