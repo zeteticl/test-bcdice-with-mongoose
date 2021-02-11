9066d2ff6f6a837574dd4d43c6c0b45d57db43df
Zeteticl <zetet@dasd.com>
2021-02-12 00:14:24 +0800

    x

--- index.js
+++ index.js
@@ -15,6 +15,19 @@ async function main() {
 
 
 
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 
 
 //npm install mongodb --save
@@ -28,11 +41,12 @@ const url = 'mongodb+srv://testtest:testtest@zetetic.lxckb.mongodb.net/test?retr
 const dbName = 'test';
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function (err, client) {
+MongoClient.connect(url, async function (err, client) {
   assert.equal(null, err);
   console.log("Connected successfully to server");
 
   const db = client.db(dbName);
+  client.login('token');
+  await main();
 
-  main();
 });
